1f6bf29398458b4ca7d1450926a187ddf6820cc8
Mutai Hillary <hillaryksure@gmail.com>
2017-05-29 16:46:06 +0300

    main activity

--- app/src/main/java/com/example/android/knowmyclub/MainActivity.java
+++ app/src/main/java/com/example/android/knowmyclub/MainActivity.java
@@ -7,17 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
 
+
 public class MainActivity extends AppCompatActivity {
 
-    private RadioGroup radioGroupCoach;
-    private RadioButton radioButtonWenger;
-    private RadioButton radioButtonMourinho;
-    private RadioButton radioButtonSir;
-    private Button buttonSubmit;
+    private RadioGroup radioCoachGroup;
+    private RadioButton radioCoachButton;
+    private CheckBox checkBoxCarling, checkBoxFA, checkBoxPremierLeague, checkBoxChampion;
+    private Button btnSubmit;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,30 +26,62 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         addListenerOnButton();
+        addListenerOncheckBoxCarling();
 
     }
 
     public void addListenerOnButton() {
 
-        radioGroupCoach = (RadioGroup) findViewById(R.id.radioGroupCoach);
-        buttonSubmit = (Button) findViewById(R.id.buttonSubmit);
+        radioCoachGroup = (RadioGroup) findViewById(R.id.radioCoach);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        checkBoxCarling = (CheckBox) findViewById(R.id.checkBoxCarling);
+        checkBoxFA = (CheckBox) findViewById(R.id.checkBoxFA);
+        checkBoxPremierLeague = (CheckBox) findViewById(R.id.checkBoxPremierLeague);
+        checkBoxChampion = (CheckBox) findViewById(R.id.checkBoxChampion);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
 
-        buttonSubmit.setOnClickListener(new OnClickListener() {
+        btnSubmit.setOnClickListener(new OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
                 // get selected radio button from radioGroup
-                int selectedId = radioGroupCoach.getCheckedRadioButtonId();
+                int selectedId = radioCoachGroup.getCheckedRadioButtonId();
 
                 // find the radiobutton by returned id
-                radioGroupCoach = (RadioGroup) findViewById(selectedId);
+                radioCoachButton = (RadioButton) findViewById(selectedId);
 
                 Toast.makeText(MainActivity.this,
-                        radioGroupCoach.getText(), Toast.LENGTH_SHORT).show();
+                        radioCoachButton.getText(), Toast.LENGTH_SHORT).show();
 
             }
 
+        });
+    }
+
+    public void addListenerOncheckBoxCarling() {
+
+        checkBoxCarling = (CheckBox) findViewById(R.id.checkBoxCarling);
+
+        checkBoxCarling.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //is checkBoxCarling checked?
+                if (((CheckBox) v).isChecked()) {
+                    Toast.makeText(MainActivity.this,
+                            "Yes, In 2001,2004,2007,2014,2015)", Toast.LENGTH_LONG).show();
+                }
+
+                StringBuffer result = new StringBuffer();
+                result.append("Carling Cup check : ").append(checkBoxCarling.isChecked());
+                result.append("\nFA Cup check : ").append(checkBoxFA.isChecked());
+                result.append("\nPremier League check :").append(checkBoxPremierLeague.isChecked());
+                result.append("\nChampion League check :").append(checkBoxChampion.isChecked());
+
+                Toast.makeText(MainActivity.this, result.toString(),
+                        Toast.LENGTH_LONG).show();
+            }
         });
 
     }
